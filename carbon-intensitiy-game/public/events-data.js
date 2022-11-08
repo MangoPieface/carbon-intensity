9b41e575ad6e5dea38a2e8c36d4622335c296711
Adam Jenkin <ajenkin@zonedigital.com>
2022-11-08 11:15:20 +0000

    Added events.js and associated images. still more to come.

---
+++ carbon-intensitiy-game/public/events-data.js
@@ -0,0 +1,64 @@
+{
+    events[
+        {
+            "id" : 1,
+            "name" : "Christmas day 2021",
+            "date" : "2022-12-25",
+            "image" : "images/christmas.jpg",
+        },
+        {
+            "id" : 2,
+            "name" : "Boxing day 2021",
+            "date" : "2022-12-25",
+            "image" : "",
+        },  
+        {
+            "id" : 3,
+            "name" : "NYE 2021",
+            "date" : "2021-12-31",
+            "image" : "images/nyew2021.jpg",
+        },  
+        {
+            "id" : 4,
+            "name" : "Queens funeral",
+            "date" : "2021-09-19",
+            "image" : "images/qe2",
+        },  
+        {
+            "id" : 5,
+            "name" : "Mens FA Cup Final",
+            "date" : "2022-05-14",
+            "image" : "",
+        },  
+        {
+            "id" : 6,
+            "name" : "Womens Euros Cup Final 2022", 
+            "date" : "2022-07-31",
+            "image" : "",
+        },  
+        {
+            "id" : 7,
+            "name" : "Grand national 2022", 
+            "date" : "2022-04-09",
+            "image" : "",
+        },  
+        {
+            "id" : 8,
+            "name" : "Mens Wimbledon final 2022", 
+            "date" : "2022-07-10",
+            "image" : "",
+        },  
+        {
+            "id" : 9,
+            "name" : "Womens Wimbledon final 2022", 
+            "date" : "2022-07-09",
+            "image" : "",
+        },  
+        {
+            "id" : 10,
+            "name" : "London Marathon 2022", 
+            "date" : "2022-10-02",
+            "image" : "",
+        }
+    ]
+}
