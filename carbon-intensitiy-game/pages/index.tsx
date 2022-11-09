3f9134d351ed26593f878e7954736593879d5eec
Steven Newstead <steven.newstead@zonedigital.com>
2022-11-09 10:59:58 +0000

    fix the link in the footer

--- carbon-intensitiy-game/pages/index.tsx
+++ carbon-intensitiy-game/pages/index.tsx
@@ -122,6 +122,7 @@ export default function Home({ data }) {
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
+          rel="noopener noreferrer"
         >
           https://api.carbonintensity.org.uk/
         </a>
